ead7db1f303fd7a21384698b1f57b25095a92e57
Juan <jm.arango03@gmail.com>
2025-02-02 21:33:17 -0500

    update RegisterRequest validation and change version to 0.0.9

--- src/main/java/me/jmarango/security/dto/request/RegisterRequest.java
+++ src/main/java/me/jmarango/security/dto/request/RegisterRequest.java
@@ -2,23 +2,27 @@ package me.jmarango.security.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Validated
 public class RegisterRequest {
 
     @Schema(minLength = 4, maxLength = 20, requiredMode = Schema.RequiredMode.REQUIRED)
-    @Length(min = 4, max = 20)
+    @Pattern(regexp = "^[a-zA-Z0-9_-]+$")
+    @Size(min = 4, max = 20)
     @NotBlank
-    private String username;
+    protected String username;
 
     @Schema(minLength = 4, requiredMode = Schema.RequiredMode.REQUIRED)
-    @Length(min = 4)
+    @Size(min = 4)
     @NotBlank
-    private String password;
+    protected String password;
 }
