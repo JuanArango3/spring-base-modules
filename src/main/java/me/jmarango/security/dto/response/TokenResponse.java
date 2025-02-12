3cc7c2b28d72f7d5d98c081a9071f40c3b6fa1a9
Juan <jm.arango03@gmail.com>
2025-02-12 15:04:47 -0500

    update TokenResponse to include refreshToken and remove username and userType fields

--- src/main/java/me/jmarango/security/dto/response/TokenResponse.java
+++ src/main/java/me/jmarango/security/dto/response/TokenResponse.java
@@ -6,7 +6,6 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class TokenResponse {
-    private String username;
-    private String userType;
     private String token;
+    private String refreshToken;
 }
