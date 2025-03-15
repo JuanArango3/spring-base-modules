a9761934cc4eaef1ae23fa88c78ffd2b46b32da2
Juan <jm.arango03@gmail.com>
2025-03-15 18:12:32 -0500

    refactor PageInfo to change totalElements type to int and add constructor for easier initialization

--- src/main/java/me/jmarango/base/dto/response/page/PageInfo.java
+++ src/main/java/me/jmarango/base/dto/response/page/PageInfo.java
@@ -2,19 +2,21 @@ package me.jmarango.base.dto.response.page;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PageInfo<T> {
     private List<T> elements;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private Long totalElements;
+    private long totalElements;
 
     private int actualPage;
     private int totalPages;
